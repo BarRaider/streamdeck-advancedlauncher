5e5bd2841ec463759bde73f1a2a8234ac67398ad
BarRaider <46548278+BarRaider@users.noreply.github.com>
2024-12-29 13:00:59 +0200

    Update dependencies

--- streamdeck-advancedlauncher/Program.cs
+++ streamdeck-advancedlauncher/Program.cs
@@ -1,10 +1,5 @@
-﻿using AdvancedLauncher.Backend;
-using BarRaider.SdTools;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BarRaider.SdTools;
+using BRUtils;
 
 namespace AdvancedLauncher
 {
